5e2738963ff9aa3ad67518818a3a707c5f84c936
Vo Dang Trinh <vodangtrinh22@gmail.com>
2026-05-12 15:54:02 +0700

    nhúng boostrap 4 và nhúng file tĩnh

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+
 const route = require("./routes/client/index.route");
 
 const app = express();
@@ -7,6 +8,8 @@ const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static("public"));
 
 route(app);
 
