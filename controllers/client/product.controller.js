1d943ae0c851369241f47cf363c214b52ccd3b42
Vo Dang Trinh <vodangtrinh22@gmail.com>
2026-05-13 09:36:05 +0700

    sửa layout và fetch dữ liệu sản phẩm vào

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
-  res.render("client/pages/products/index.pug", {
-    pageTitle: "Sản phẩm",
-  });
+module.exports.index = async (req, res) => {
+  try {
+    const response = await fetch("https://dummyjson.com/products");
+    const data = await response.json();
+
+    res.render("client/pages/products/index.pug", {
+      pageTitle: "Sản phẩm",
+      products: data.products || [],
+    });
+  } catch (error) {
+    console.error("Không lấy được dữ liệu sản phẩm:", error);
+
+    res.render("client/pages/products/index.pug", {
+      pageTitle: "Sản phẩm",
+      products: [],
+    });
+  }
 };
