5223892f37d7a493916aad35ec9ddc08f8aa97c3
Vo Dang Trinh <vodangtrinh22@gmail.com>
2026-05-14 15:44:38 +0700

    tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,13 +2,47 @@ const Product = require("../../models/product.model");
 
 //[GET] /admin/dashboard
 module.exports.index = async (req, res) => {
-  const products = await Product.find({
+  let fiterStatus = [
+    {
+      name: "Tất cả",
+      status: "",
+      class: "",
+    },
+    {
+      name: "Hoạt động",
+      status: "active",
+      class: "",
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: "",
+    },
+  ];
+
+  if (req.query.status) {
+    const index = fiterStatus.findIndex(
+      (item) => item.status == req.query.status,
+    );
+    fiterStatus[index].class = "active";
+  } else {
+    const index = fiterStatus.findIndex((item) => item.status == "");
+    fiterStatus[index].class = "active";
+  }
+
+  let find = {
     deleted: false,
-  });
-  console.log(products);
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  const products = await Product.find(find);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang tổng quan",
     products: products,
+    fiterStatus: fiterStatus,
   });
 };
